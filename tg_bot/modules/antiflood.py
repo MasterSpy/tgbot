6573501e9569799893ca663d26e31fa0ad9f79b7
Garcia <not@telling.you>
2018-04-28 04:48:27 -0300

    Make antiflood ban more polite

--- tg_bot/modules/antiflood.py
+++ tg_bot/modules/antiflood.py
@@ -35,8 +35,7 @@ def check_flood(bot: Bot, update: Update) -> str:
 
     try:
         chat.kick_member(user.id)
-        msg.reply_text("I like to leave the flooding to natural disasters. But you, you were just a "
-                       "disappointment. Get out.")
+        msg.reply_text("I banned this user for flooding.")
 
         return "<b>{}:</b>" \
                "\n#BANNED" \
